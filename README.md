610bf3baaef41e86c742704aa4c7a433454bc259
RaphaelDaubelcour <daubelcour63@gmail.com>
2024-07-11 02:14:30 -0400

    Add tree selection feature + code refractoring

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 After activating the addon, go to the RestPoseBinder panel, then locate the folder with the animations you want to bind (Child folders will be taken into account). Next, locate the animation. Then press bind !
 
 ![image](https://github.com/Raftatul/RestPoseBinder/assets/80292778/ede268fa-172b-4c96-adb7-d6efeff9e131)
-
--- addons/rest_pose_binder/binder.gd
+++ addons/rest_pose_binder/binder.gd
@@ -1,7 +1,13 @@
 @tool
 extends Control
 
-var valid_files: PackedStringArray
+
+@export var directory_path_input: LineEdit
+@export var animation_path_input: LineEdit
+@export var tree: Tree
+
+var valid_files := []
+var pre_root_checked := false
 
 
 func _is_file_valid(section, config) -> bool:
@@ -10,9 +16,9 @@ func _is_file_valid(section, config) -> bool:
 
 func _set_rest_pos(file_path: String, rest_pos_animation_path: String) -> int:
 	var config := ConfigFile.new()
-	var raw_path := file_path.replace(".import", "")
+	var import_path := file_path.replace(".import", "") + ".import" #Make sure to not have duplicate of .import
 	
-	var err = config.load(file_path)
+	var err = config.load(import_path)
 	
 	if err != OK:
 		print("Error on loading file ", file_path)
@@ -23,50 +29,80 @@ func _set_rest_pos(file_path: String, rest_pos_animation_path: String) -> int:
 			if not _is_file_valid(section, config):
 				printerr("File is not valid: ", file_path)
 				break
-				
 		if section == "params":
 			var _subresources = config.get_value(section, "_subresources")
 			if _subresources.is_empty():
-				printerr("Error while readind import file. Make sure to reimport correctly ", raw_path)
+				printerr("Error while readind import file. Make sure to reimport correctly ", file_path)
 				return FAILED
 			
-			_subresources["nodes"]["PATH:Skeleton3D"]["rest_pose/load_pose"] = 2
-			_subresources["nodes"]["PATH:Skeleton3D"]["rest_pose/external_animation_library"] = load(rest_pos_animation_path)
+			var skeleton = ""
+			if not _subresources["nodes"].has("PATH:Skeleton3D"):
+				skeleton = _subresources["nodes"]["PATH:RootNode/Skeleton3D"]
+			else:
+				skeleton = _subresources["nodes"]["PATH:Skeleton3D"]
+			
+			skeleton["rest_pose/load_pose"] = 2
+			skeleton["rest_pose/external_animation_library"] = load(rest_pos_animation_path)
 			
 			config.set_value(section, "_subresources", _subresources)
 			config.set_value(section, "nodes/import_as_skeleton_bones", true)
 			
-			config.save(file_path)
+			config.save(import_path)
 			break
 	
-	valid_files.append(raw_path)
-	print_rich("[color=green]Succesfully convert {file_path}[/color]".format([["file_path", raw_path]]))
+	print_rich("[color=green]Succesfully convert {file_path}[/color]".format([["file_path", file_path]]))
 	return OK
 
 
-func _convert_directory(path: String, rest_pos_animation_path: String):
-	var dir := DirAccess.open(path)
+func _update_tree(path: String) -> void:
+	var paths = [path]
+	tree.clear()
+	valid_files.clear()
 	
-	dir.list_dir_begin()
-	var file := dir.get_next()
-	
-	while file != "":
-		if dir.current_is_dir():
-			_convert_directory(path + file + "/", rest_pos_animation_path)
+	while paths.size() > 0:
+		var current_path = paths[0]
+		var dir := DirAccess.open(current_path)
+		dir.list_dir_begin()
+		var file := dir.get_next()
 		
-		if file.contains(".import"):
-			var import_path = path + file
-			var err := _set_rest_pos(import_path, rest_pos_animation_path)
+		var tree_parent = tree.create_item()
+		tree_parent.set_cell_mode(0, 1)
+		tree_parent.set_editable(0, true)
+		tree_parent.set_text(0, current_path)
+		tree_parent.propagate_check(0)
+		
+		while file != "":
+			if dir.current_is_dir():
+				paths.append(current_path + file + "/")
+			elif file.ends_with(".import"):
+				var child = tree.create_item(tree_parent)
+				child.set_cell_mode(0, 1)
+				child.set_editable(0, true)
+				child.set_text(0, file)
 			
-			if err != OK:
-				printerr("Error while converting animations ", import_path)
-				break
+			file = dir.get_next()
 		
-		file = dir.get_next()
+		if tree_parent.get_child_count() == 0:
+			tree_parent.free()
 		
-	dir.list_dir_end()
+		dir.list_dir_end()
+		paths.remove_at(0)
+	
+	var root = tree.get_root()
+	if root:
+		root.set_checked(0, true)
+		root.propagate_check(0)
 
 
+func _populate_valid_path(item: TreeItem, column: int, path: String) -> void:
+	if item.is_checked(column) and not valid_files.has(path):
+		valid_files.append(path)
+	else:
+		valid_files.erase(path)
+	print(valid_files)
+
+
+#Inputs
 func _on_folder_btn_pressed() -> void:
 	$FileDialog.visible = true
 
@@ -75,17 +111,18 @@ func _on_animation_btn_pressed() -> void:
 	$ResPosPathDialog.visible = true
 
 
-func _on_file_dialog_dir_selected(dir: String) -> void:
-	%Directory.text = dir
+func _on_file_dialog_dir_selected(path: String) -> void:
+	directory_path_input.text = path
+	_update_tree(path)
 
 
 func _on_res_pos_path_dialog_file_selected(path: String) -> void:
-	%RestPosePath.text = path
+	animation_path_input.text = path
 
 
 func _on_button_pressed() -> void:
-	var path = %Directory.text
-	var rest_pos_animation_path = %RestPosePath.text
+	var path = directory_path_input.text
+	var rest_pos_animation_path = animation_path_input.text
 	
 	var dir = DirAccess.open(path)
 	
@@ -97,7 +134,31 @@ func _on_button_pressed() -> void:
 		printerr("Error while opening the rest pos animation path")
 		return
 	
-	_convert_directory(path, rest_pos_animation_path)
+	for p in valid_files:
+		_set_rest_pos(p, rest_pos_animation_path)
 	
 	EditorInterface.get_resource_filesystem().reimport_files(valid_files)
-	valid_files.clear()
+
+
+#Tree selection
+func _on_tree_item_edited() -> void:
+	var column = tree.get_edited_column()
+	tree.get_edited().propagate_check(column)
+
+
+func _on_tree_check_propagated_to_item(item: TreeItem, column: int) -> void:
+	if item == tree.get_root():
+		return
+	
+	var path = item.get_parent().get_text(column) + item.get_text(column)
+	if path.ends_with(".import"):
+		path = path.replace(".import", "")
+		_populate_valid_path(item, column, path)
+
+
+func _on_refresh_tree_btn_pressed() -> void:
+	if directory_path_input.text.is_empty():
+		tree.clear()
+		return
+	
+	_update_tree(directory_path_input.text)
